6be258fa192db3b27b8ae5224c2583d5ea302389
Chetan Gawade <chetan@newput.com>
2017-08-28 15:53:58 +0530

    User working 100% - except security

--- app/Photo.php
+++ app/Photo.php
@@ -6,7 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+    protected $upload = '/images/';
+
     protected $fillable = [
         'file'
     ];
+
+    public function getFileAttribute($photo)
+    {
+    	return $this->upload.$photo;
+    }
+
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,6 +6,7 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -19,8 +20,9 @@
 
     		@foreach($users as $user)
 		      <tr>
-		        <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
+            <td>{{$user->id}}</td>
+		        <td><image height="50" src="{{ $user->photo ? $user->photo->file : 'http://www.placehold.it/400x400'}}"</td>
+            <td><a href="{{route('admin.users.edit',$user->id)}}">{{$user->name}}</a></td>
             <td>{{$user->email}}</td>
 		        <td>{{$user->role->name}}</td>
 		        <td>{{$user->is_active == 1 ? 'Active' : 'Deactive'}}</td>
