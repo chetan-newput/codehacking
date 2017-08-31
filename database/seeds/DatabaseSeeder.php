e243f96ac59827a380f9c62ba8df79f983202459
Chetan Gawade <chetan@newput.com>
2017-08-31 17:31:15 +0530

    DB:Seeding working

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Category;
+use App\Comment;
+use App\CommentReply;
+use App\Photo;
+use App\Post;
+use App\Role;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +18,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+    	DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+    	DB::table('users')->truncate();
+
+    	DB::table('posts')->truncate();
+
+    	DB::table('photos')->truncate();
+
+    	DB::table('categories')->truncate();
+
+    	DB::table('roles')->truncate();
+
+    	DB::table('comments')->truncate();
+
+    	DB::table('comment_replies')->truncate();
+    
+      factory(User::class, 10)->create()->each(function($user){
+      	$user->posts()->save(factory(Post::class)->make());
+      });
+
+      factory(Role::class, 3)->create();
+
+      factory(Category::class, 10)->create();
+      
+      factory(Photo::class, 1)->create();
+
+      factory(Comment::class, 10)->create()->each(function($comment){
+      	$comment->replies()->save(factory(CommentReply::class)->make());
+      });
+      
+      // $this->call(UsersTableSeeder::class);
     }
 }
